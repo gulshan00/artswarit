ba6ed9a79a4ec7ded6f1492c59e2f0ae44b89033
gulshan.pairbytes <gulshan.pairbytes@gmail.com>
2025-02-19 21:16:33 +0530

    initial commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './Components/Auths/signup';
-import Login from './Components/Auths/login';
+import Login from './Components/Auths/Login';
 import Home from './Components/Home/Home';
 function App() {
   return (
